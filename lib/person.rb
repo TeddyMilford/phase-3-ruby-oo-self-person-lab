b6fae1d559cc326fab20db79af686c8be25767c6
Teddy Milford <teddy.milford@gmail.com>
2022-06-27 19:45:22 -0600

    commit tree e9c5aec992a2bf7bfe0f5c8c98baf73f42342efb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,40 @@
 # your code goes here
+
+class Person
+    attr_accessor :bank_account, 
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    self.name
+  end
+
+  def bank_account
+    self.bank_account
+  end
+
+  def bank_account = (value)
+    @bank_account = value
+  end
+
+  def happiness
+    self.happiness
+  end
+
+  def happiness = (value)
+    @happiness = value
+  end
+
+  def hygeine
+    self.hygeine
+  end
+
+  def hygeine = (value)
+    @hygeine = value
+  end
+  
+
+
+end
